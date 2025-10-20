c03b6992b22e0c439cac9d778065011bafc500c8
Anton Abyshev <abyshev.anton@gmail.com>
2025-10-20 15:54:51 +0400

    add hashtags

--- db/models.py
+++ db/models.py
@@ -159,7 +159,40 @@ class Author(Base):
     cascade="all, delete-orphan",
     passive_deletes=True,
   )
-  
+
+
+class Hashtag(Base):
+  __tablename__ = "hashtags"
+
+  id: Mapped[uuid.UUID] = mapped_column(
+    UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v1mc()")
+  )
+  source: Mapped[VideoSource] = mapped_column(
+    Enum(VideoSource, name="hashtag_source", native_enum=True), nullable=False
+  )
+  name: Mapped[str] = mapped_column(String(512), nullable=False, index=True)
+  is_reviewed: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text("false"))
+
+  created_at: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True),
+    default=func.now(), nullable=False
+  )
+  updated_at: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True),
+    default=func.now(), onupdate=func.now(), nullable=False
+  )
+
+  video_hashtags: Mapped[list["VideoHashtag"]] = relationship(
+    back_populates="hashtag",
+    cascade="all, delete-orphan",
+    passive_deletes=True,
+  )
+  videos: Mapped[list["Video"]] = relationship(
+    secondary="video_hashtags",
+    back_populates="hashtags",
+    viewonly=True,
+  )
+
 
 class Video(Base):
   """
@@ -231,6 +264,16 @@ class Video(Base):
     back_populates="videos",
     viewonly=True,
   )
+  video_hashtags: Mapped[list["VideoHashtag"]] = relationship(
+    back_populates="video",
+    cascade="all, delete-orphan",
+    passive_deletes=True,
+  )
+  hashtags: Mapped[list["Video"]] = relationship(
+    secondary="video_hashtags",
+    back_populates="videos",
+    viewonly=True,
+  )
   video_topics: Mapped[list["VideoTopic"]] = relationship(
     back_populates="video",
     cascade="all, delete-orphan",
@@ -241,7 +284,31 @@ class Video(Base):
     back_populates="videos",
     viewonly=True,
   )
-  
+
+
+class VideoHashtag(Base):
+  __tablename__ = "video_hashtags"
+
+  hashtag_id: Mapped[uuid.UUID] = mapped_column(
+    UUID(as_uuid=True),
+    ForeignKey("hashtags.id", ondelete="CASCADE"),
+    nullable=False,
+    primary_key=True,
+  )
+  video_id: Mapped[uuid.UUID] = mapped_column(
+    UUID(as_uuid=True),
+    ForeignKey("videos.id", ondelete="CASCADE"),
+    nullable=False,
+    primary_key=True,
+  )
+  created_at: Mapped[datetime] = mapped_column(
+    DateTime(timezone=True),
+    default=func.now(), nullable=False
+  )
+
+  topic: Mapped["Hashtag"] = relationship(back_populates="video_hashtags")
+  video: Mapped["Video"] = relationship(back_populates="video_hashtags")
+
 
 class VideoTopic(Base):
   __tablename__ = "video_topics"
