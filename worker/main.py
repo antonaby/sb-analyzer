abc563f857a9e64e8db462a4f5fa60aebb9197ec
Anton Abyshev <abyshev.anton@gmail.com>
2025-10-27 16:04:47 +0400

    correct fix of logfire

--- worker/main.py
+++ worker/main.py
@@ -1,11 +1,20 @@
 import os
 
+import logfire
 from celery import Celery
+from celery.signals import worker_init
 from dotenv import load_dotenv
 
 load_dotenv()
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 CELERY_BACKEND_URL = os.getenv("CELERY_BACKEND_URL")
+
+@worker_init.connect()
+def init_worker(*args, **kwargs):
+  logfire.configure(service_name="worker")
+  logfire.instrument_celery()
+  logfire.instrument_pydantic_ai()
+
 
 worker_app = Celery(
   "video-analyzer",
--- worker/tasks/worker.py
+++ worker/tasks/worker.py
@@ -3,14 +3,8 @@ from celery.signals import worker_process_init, worker_shutting_down
 
 @worker_process_init.connect
 def init_worker_process(**kwargs):
-  import logfire
-
   from worker.tasks.deps import loop, async_db
   from db.conf import test_db_conn_with_session
-
-  logfire.configure(service_name="worker")
-  logfire.instrument_pydantic_ai()
-  logfire.instrument_celery()
 
   loop.run_until_complete(test_db_conn_with_session(async_db))
 
