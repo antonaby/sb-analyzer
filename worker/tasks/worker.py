ec255448cf9eedcda1f2d1018d91d01c6e299ae4
Anton Abyshev <abyshev.anton@gmail.com>
2025-10-27 15:54:02 +0400

    fix logfire

--- worker/tasks/worker.py
+++ worker/tasks/worker.py
@@ -1,18 +1,16 @@
-import logfire
-from celery.signals import worker_init, worker_process_init, worker_shutting_down
-
-
-@worker_init.connect()
-def init_worker(*args, **kwargs):
-  logfire.configure(service_name="worker")
-  logfire.instrument_celery()
-  logfire.instrument_pydantic_ai()
+from celery.signals import worker_process_init, worker_shutting_down
 
 
 @worker_process_init.connect
 def init_worker_process(**kwargs):
+  import logfire
+
   from worker.tasks.deps import loop, async_db
   from db.conf import test_db_conn_with_session
+
+  logfire.configure(service_name="worker")
+  logfire.instrument_pydantic_ai()
+  logfire.instrument_celery()
 
   loop.run_until_complete(test_db_conn_with_session(async_db))
 
