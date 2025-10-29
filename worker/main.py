e8b9f92a98010b262309eb9b626e7b5351da8888
Anton Abyshev <abyshev.anton@gmail.com>
2025-10-29 11:08:53 +0400

    add beat to celery

--- worker/main.py
+++ worker/main.py
@@ -2,7 +2,8 @@ import os
 
 import logfire
 from celery import Celery
-from celery.signals import worker_init
+from celery.schedules import crontab
+from celery.signals import worker_init, beat_init
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,6 +13,13 @@ CELERY_BACKEND_URL = os.getenv("CELERY_BACKEND_URL")
 @worker_init.connect()
 def init_worker(*args, **kwargs):
   logfire.configure(service_name="worker")
+  logfire.instrument_celery()
+  logfire.instrument_pydantic_ai()
+
+
+@beat_init.connect()
+def init_beat(*args, **kwargs):
+  logfire.configure(service_name="beat")
   logfire.instrument_celery()
   logfire.instrument_pydantic_ai()
 
@@ -26,4 +34,11 @@ worker_app.conf.result_backend_transport_options = {
   'global_keyprefix': 'sb-analyzer:'
 }
 
+worker_app.conf.timezone = 'UTC'
 worker_app.conf.imports = ["worker.tasks"]
+worker_app.conf.beat_schedule = {
+  'run_scrapers': {
+    'task': 'worker.tasks.scrapers.run_scrapers',
+    'schedule': crontab(hour=4, minute=0),
+  },
+}
